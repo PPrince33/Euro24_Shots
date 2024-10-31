b6203f4279118ec4ff8af8555b4efb58dcb20fa3
PPrince33 <137063047+PPrince33@users.noreply.github.com>
2024-11-01 00:37:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ if match:
             plt.scatter([], [], color='green', marker='o', label='Attackers')
             plt.scatter([], [], color='red', marker='o', label='Defenders')
             
-            plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.1), ncol=1, frameon=False, title="Legend")
+            plt.legend(loc="upper right", bbox_to_anchor=(0.5, 1.1), ncol=1, frameon=True, title="Legend")
             # Convert plot to image for Streamlit display
             buf = io.BytesIO()
             fig.savefig(buf, format="png", facecolor='black')
